956071feac5e3563240fb7d858a6832521839f38
spfox24 <68411909+spfox24@users.noreply.github.com>
2023-03-23 11:25:37 -0700

    Fetch the genres

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -22,26 +22,26 @@ export interface Game {
   }
 
 const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
 
-const [isLoading, setLoading] = useState(false)
+  const [isLoading, setLoading] = useState(false)
   
-useEffect(() => {
-    const controller = new AbortController();
-    
-    setLoading(true);
-      apiClient
-        .get<FetchGamesResponse>("/games", {signal: controller.signal})
-        .then((res) => {
-          setGames(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
+  useEffect(() => {
+      const controller = new AbortController();
+      
+      setLoading(true);
+        apiClient
+          .get<FetchGamesResponse>("/games", {signal: controller.signal})
+          .then((res) => {
+            setGames(res.data.results);
             setLoading(false);
-          });
+          })
+          .catch((err) => {
+              if (err instanceof CanceledError) return;
+              setError(err.message)
+              setLoading(false);
+            });
 
         return () => controller.abort();
     }, []);
